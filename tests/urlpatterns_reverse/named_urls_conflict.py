518d27f1135a526c08aaa2604f95d30982183950
Reinout van Rees <reinout@vanrees.org>
2016-11-05 12:55:48 +0100

    Little cleanup
    This fixes a comment I had on
    https://github.com/django/django/pull/7417/files

--- tests/urlpatterns_reverse/named_urls_conflict.py
+++ tests/urlpatterns_reverse/named_urls_conflict.py
@@ -3,16 +3,15 @@ from django.conf.urls import url
 from .views import empty_view
 
 urlpatterns = [
-    # no args
+    # No kwargs
     url(r'^conflict/$', empty_view, name="name-conflict"),
 
-    # 1 arg
+    # One kwarg
     url(r'^conflict-first/(?P<first>\w+)/$', empty_view, name="name-conflict"),
     url(r'^conflict-cannot-go-here/(?P<middle>\w+)/$', empty_view, name="name-conflict"),
     url(r'^conflict-middle/(?P<middle>\w+)/$', empty_view, name="name-conflict"),
     url(r'^conflict-last/(?P<last>\w+)/$', empty_view, name="name-conflict"),
 
-    # 2 args
+    # Two kwargs
     url(r'^conflict/(?P<extra>\w+)/(?P<another>\w+)/$', empty_view, name="name-conflict"),
-
 ]
